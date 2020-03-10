71b78bbe655adc058d0719d63ffc53aa5cac22b9
DoubleStackedMind <sami.asfouri@esprit.tn>
2020-03-10 13:42:39 +0100

    Update CaesersCipher.java

--- caesars-cipher/src/main/java/CaesersCipher.java
+++ caesars-cipher/src/main/java/CaesersCipher.java
@@ -4,7 +4,7 @@ public class CaesersCipher {
 	private static final char LETTER_A = 'a';
 	private static final char LETTER_Z = 'z';
 	private static final int ALPHABET_SIZE = 26;
-
+//ss
 	public String cipher(String message, int offset) {
 		offset %= ALPHABET_SIZE;
 		char[] character = message.toCharArray();
